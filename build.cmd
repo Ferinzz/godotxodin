40c45b3afca79477aa04530747172d6fc8ca9703
Benjamin Gould <44227010+Ferinzz@users.noreply.github.com>
2025-05-21 12:48:23 +0300

    Add files via upload

--- build.cmd
+++ build.cmd
@@ -2,11 +2,17 @@
 ::echo on
 godot --headless --dump-gdextension-interface
 godot --headless --dump-extension-api
-::odin run .
+
+::runs whatever odin main package there is in the folder you're in.
+::probably better to have the main file in a src directory or something, but I wasn't planning on having more than one file at the start.
 odin build . -build-mode:dll
 
 rename GDExtension.dll libgdexample.dll
-::del .\bin\libgdexample.dll
+
+::Change this to a folder in the project you're running.
+::Don't forget to put the gdexample.gdextension file somewhere in the project. Not sure if it matters where.
 xcopy /v /i /Y libgdexample.dll .\testextension\bin\
 del libgdexample.dll
+
+::Change this to where you have your Godot Project file.
 godot .\testextension\project.godot
